2ea589c55db905893f5bc68e10d4cfdeb862930d
Paulina <paulina.derkowska@huuugegames.com>
2021-04-20 19:11:57 +0200

    Setup Store Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import {UserModule} from './user/user.module';
-import {EffectsModule} from '@ngrx/effects';
-import {reducer} from './user/store/users.reducer';
-import {FormsModule} from '@angular/forms';
+import { UserModule } from './user/user.module';
+import { EffectsModule } from '@ngrx/effects';
+import { reducer } from './user/store/users.reducer';
+import { FormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import {FormsModule} from '@angular/forms';
     UserModule,
     FormsModule,
     StoreModule.forRoot({ users: reducer }, {}),
+    StoreDevtoolsModule.instrument(),
     EffectsModule.forRoot([]),
   ],
   providers: [],
