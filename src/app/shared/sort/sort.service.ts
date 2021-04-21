5d15a471749a45b92a344541320f63ccf0bffb91
Paulina <paulina.derkowska@huuugegames.com>
2021-04-21 14:59:21 +0200

    Fix sorting

--- src/app/shared/sort/sort.service.ts
+++ src/app/shared/sort/sort.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { ReplaySubject, Subject } from 'rxjs';
 import { ColumnSortedData } from './model/column-sorted-data.model';
 
 @Injectable()
 export class SortService {
 
-  private readonly columnSortedSubject = new Subject<ColumnSortedData>();
+  private readonly columnSortedSubject = new ReplaySubject<ColumnSortedData>(1);
 
   columnSorted$ = this.columnSortedSubject.asObservable();
 
